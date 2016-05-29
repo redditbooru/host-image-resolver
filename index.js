5c308f07e1f60da2c37c30324278063c5ac82ac0
Matt Hackmann <matt@dxprog.com>
2016-05-29 12:16:05 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const parse = require('url').parse;
+
+// Set up a global for module caching
+const cache = global._hostImageResolver = global._hostImageResolver || {};
+
+/**
+ * Returns an array of resolved image URLs from a passed in URL
+ *
+ * @param {String} url The URL to resolve the images from
+ * @returns {Promise}  An promise that resolves with an array of
+ *                     image URLs. If the URL does not resolve to
+ *                     a host, returns the original URL in the array.
+ */
+module.exports = function(url) {
+  return new Promise((resolve, reject) => {
+    const urlPieces = parse(url);
+
+    if (urlPieces.host) {
+      // We're going to throw away the subdomain
+      const domainPieces = urlPieces.host.split('.');
+      if (domainPieces.length === 3) {
+        domainPieces.shift();
+      }
+
+      // The service name is host-tld. Ex: imgur-com. Because
+      // you never know when imgur-jp will show up and ruin the party...
+      const serviceName = domainPieces.join('-').toLowerCase();
+      try {
+        const service = cache[serviceName] || require(`./service-connectors/${serviceName}`);
+        cache[serviceName] = service;
+        retVal = service(urlPieces).then(resolve);
+        retVal = Array.isArray(retVal) ? retVal : [ retVal ];
+      } catch (exc) {
+        console.error(`Unable to find service connector for ${urlPieces.host}`);
+        resolve([ url ]);
+      }
+    }
+  });
+};
---
+++ service-connectors/deviantart-com.js
@@ -0,0 +1,16 @@
+const format = require('url').format;
+const request = require('request');
+
+module.exports = function(urlPieces) {
+  return new Promise((resolve, reject) => {
+    const apiCall = `http://backend.deviantart.com/oembed?url=${encodeURIComponent(format(urlPieces))}`;
+    request(apiCall, (err, res, body) => {
+      try {
+        const data = JSON.parse(body);
+        !!data.url ? resolve(data.url) : reject();
+      } catch (exc) {
+        reject(`Server returns an invalid response [HTTP ${res.code}]`);
+      }
+    });
+  });
+};
